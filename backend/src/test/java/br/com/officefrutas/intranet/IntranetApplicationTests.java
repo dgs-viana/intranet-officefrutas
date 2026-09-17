208d376d0e2ad114197c02645561525b56a815a4
Felipe Viana <frvandrade@gmail.com>
2026-09-17 14:15:35 -0300

    Criação backend springboot e rota de health

---
+++ backend/src/test/java/br/com/officefrutas/intranet/IntranetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.officefrutas.intranet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntranetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
